2659080cf5575119abc069a05a451ba261a92fc7
Nasikh CL <nasikcl@gmail.com>
2023-12-05 11:34:56 +0530

    setting up redis

---
+++ server/src/utils/redis.ts
@@ -0,0 +1,9 @@
+import {Redis} from 'ioredis';
+
+const redisClient = () =>{
+    if(process.env.REDIS_URL){
+        return process.env.REDIS_URL
+    }
+    throw new Error('redis connection failed')
+}
+export const redis = new Redis(redisClient())
