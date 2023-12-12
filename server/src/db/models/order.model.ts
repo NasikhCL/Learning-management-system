33a01639414c53ab5d13cc9176122c56ba7f2ba2
Nasikh CL <nasikcl@gmail.com>
2023-12-12 20:42:31 +0530

    order model

---
+++ server/src/db/models/order.model.ts
@@ -0,0 +1,25 @@
+import mongoose, { Document, Model } from "mongoose";
+
+export interface IOrder extends Document{
+    courseId: string;
+    userId: string;
+    payment_info: object;
+}
+
+
+const orderSchema = new mongoose.Schema<IOrder>({
+    courseId:{
+        type: String,
+        required: true
+    },
+    userId:{
+        type: String,
+        required: true
+    },
+    payment_info:{
+        type: Object
+    }
+},{timestamps: true});
+
+const orderModel: Model<Iorder> = mongoose.model('Order', orderSchema);
+export default orderModel
