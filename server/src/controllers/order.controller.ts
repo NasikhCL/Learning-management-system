8c3fb67e2322b30192b46f20614a5be87b8ee6bd
Nasikh CL <nasikcl@gmail.com>
2023-12-13 20:30:29 +0530

    create order controller

---
+++ server/src/controllers/order.controller.ts
@@ -0,0 +1,78 @@
+import catchAsyncError from "../middleware/catchAsyncError";
+import ErrorHandler from "../utils/ErrorHandler";
+import {IOrder, orderModel} from '../db/models/order.model'
+import courseModel from "../db/models/course.model";
+import sendMail from "../utils/sendMail";
+import UserModel from "../db/models/user.model";
+import { redis } from "../utils/redis";
+
+
+
+
+
+
+export const createOrder = catchAsyncError(async(req, res, next)=>{
+    try {
+        const {userId, courseId, payment_info} = req.body as IOrder;
+
+        const cacheData = await redis.get(userId);
+        
+        if(!cacheData){
+            return next(new ErrorHandler("error fetching user", 400));
+        }
+        const user = JSON.parse(cacheData);
+
+        const courseExistsInUser = user.courses.some((course:string)=> courseId=== course)
+        if(courseExistsInUser){
+            return next(new ErrorHandler("course already purchased", 400));
+        }
+        const course = await courseModel.findById(courseId);
+        if(!course){
+            return next(new ErrorHandler("course not found", 400));
+        }
+
+        const data ={
+            courseId,
+            userId,
+            payment_info
+
+        }
+       
+        const order = await orderModel.create(data)
+        try{
+            const data={
+                order:{
+                    name: user.name,
+                    order_id: order._id.slice(0,6),
+                    course_name:course.title,
+                    price: course.price,
+                    date: new Date().toLocaleDateString('en-US', { year:"numeric", month:"long", day:"numeric"})
+                }
+            }
+            await sendMail({
+                email: user.email,
+                subject: "Order Successfull",
+                template: "order.email.ejs",
+                data
+            });
+        }catch(err:any){
+            return next(new ErrorHandler(err.message, 500))
+        }
+
+        user.course.push(courseId);
+        
+        const updatedUser = await UserModel.findByIdAndUpdate(userId,{user},{new:true});
+
+        await redis.set(userId, JSON.stringify(updatedUser))
+
+        return res.status(201).json({
+            success: true,
+            message: "purchase successful",
+            course
+        });
+
+        
+    } catch (err:any) {
+        return next(new ErrorHandler(err.message,400))
+    }
+})
