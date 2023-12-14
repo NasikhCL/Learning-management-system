28eac51aee82166762b97a5e20425d1044a9c0e4
Nasikh CL <nasikcl@gmail.com>
2023-12-14 10:51:13 +0530

    modified admin and course routes

--- server/src/routes/v1/adminRoutes/index.ts
+++ server/src/routes/v1/adminRoutes/index.ts
@@ -1,5 +1,4 @@
 import express from 'express';
-import { createCourse, deleteCourse, updateCourse } from '../../../controllers/course.controller';
 import { authorizeRoles, isAuthenticted } from '../../../middleware/auth';
 import { deleteUser } from '../../../controllers/user.controller';
 
@@ -9,12 +8,6 @@ const router = express.Router();
 router.get('/details', (req, res)=>{
    return res.send('Hello admin')
 });
-
-router.post("/create-course", isAuthenticted, authorizeRoles("admin"),createCourse);
-
-router.put("/update-course/:id", isAuthenticted, authorizeRoles("admin"), updateCourse);
-
-router.delete("/delete-course/:id", isAuthenticted, authorizeRoles("admin"), deleteCourse);
 
 router.delete("/delete-user/:id", isAuthenticted, authorizeRoles("admin"), deleteUser);
 
